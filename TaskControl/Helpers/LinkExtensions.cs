087ee13048d24befa0e72a6c10348752686a22c2
Fernando Oliva <fernando.oliva.hueto@gmail.com>
2014-08-19 12:33:29 +0200

    Helper extension method

---
+++ TaskControl/Helpers/LinkExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskControl.Helpers
+{
+    public static class LinkExtensions
+    {
+        /// <summary>
+        /// Extension method to do image link
+        /// </summary>
+        /// <param name="html"></param>
+        /// <param name="action"></param>
+        /// <param name="controller"></param>
+        /// <param name="imagePath"></param>
+        /// <param name="width"></param>
+        /// <param name="alt"></param>
+        /// <returns></returns>
+        public static MvcHtmlString ActionImage(this HtmlHelper html, string action, string controller, string imagePath, string width, string alt)
+        {
+            var url = new UrlHelper(html.ViewContext.RequestContext);
+
+            // build the <img> tag
+            var imgBuilder = new TagBuilder("img");
+            imgBuilder.MergeAttribute("src", url.Content(imagePath));
+            imgBuilder.MergeAttribute("alt", alt);
+            imgBuilder.MergeAttribute("width", width + "px");
+            
+            string imgHtml = imgBuilder.ToString(TagRenderMode.SelfClosing);
+
+            // build the <a> tag
+            var anchorBuilder = new TagBuilder("a");
+            anchorBuilder.MergeAttribute("href", url.Action(action, controller));
+            anchorBuilder.InnerHtml = imgHtml; // include the <img> tag inside
+            string anchorHtml = anchorBuilder.ToString(TagRenderMode.Normal);
+
+            return MvcHtmlString.Create(anchorHtml);
+        }
+    }
+}
